3c9243c6212e97b02aa7541cc7a1de589d801551
Nick20017 <56848959+Nick20017@users.noreply.github.com>
2020-05-31 12:12:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        var st = new Static(1, "Str");
-        System.out.println("a: " + st.a);
-        System.out.println("b: " + st.b);
+        var st1 = new Static(1, "Str");
+        System.out.println("a: " + st1.a);
+        System.out.println("b: " + st1.b);
+        System.out.println("sa: " + Static.sa);
+        System.out.println("sb: " + Static.sb);
+        
+        var st2 = new Static(1);
+        System.out.println("a: " + st2.a);
+        System.out.println("b: " + st2.b);
         System.out.println("sa: " + Static.sa);
         System.out.println("sb: " + Static.sb);
     }
